f14cdcc297072f1d27d7d30c3ad3d96ef323499c
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-10-23 20:46:29 -0700

    creating route folder and api file

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/todos', (req, res, next) => {
+
+});
+
+router.post('/todos', (req, res, next) => {
+
+});
+
+router.delete('/todos/:id', (req, res, next) => {
+
+})
+
+module.exports = router;
